e32d7d8a465746d7130a9f224c372c2e33fcf73a
Siegfried Dubois <dubois.siegfried@gmail.com>
2022-01-23 00:28:42 +0100

    Board base code

--- board.py
+++ board.py
@@ -1,3 +1,17 @@
 '''
 Code how the display is done
 '''
+import cell
+import random
+
+class Board:
+    def __init__(self,x,y):
+        '''
+        Board constructor
+
+        Keywords args:
+        x - number of cols
+        y - number of rows
+        '''
+        self.layout = [[cell.Cell(True if random.random()>0.3 else False) for _ in range(x)] for _ in range(y)]
+        #TODO add some things to this poor board :(
