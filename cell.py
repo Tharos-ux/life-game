e10b95230b63a89c283dbc0776adfc7c300e2603
Siegfried Dubois <dubois.siegfried@gmail.com>
2022-01-23 00:08:13 +0100

    Cell code

--- cell.py
+++ cell.py
@@ -1,3 +1,35 @@
 '''
 Define a cell
 '''
+
+class Cell:
+    def __init__(self,state=False):
+        '''
+        Cell constructor
+
+        Positionnal args:
+        state (False) - living state of celle : False means dead, True means alive
+        '''
+        self.state = False
+        #TODO add some things to this poor cell :(
+
+    def set_dead(self):
+        '''
+        setter ; cell status to dead
+        '''
+        self.state = False
+
+    def set_alive(self):
+        '''
+        setter ; cell status to alive
+        '''
+        self.state = True
+
+    def get_state(self):
+        '''
+        getter; true if cell is alive, false otherwise
+        '''
+        return self.state
+
+    def __str__(self):
+        return "O" if self.state else " "
